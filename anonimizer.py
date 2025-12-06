852d6ba639a9780bb71f300371443e2595ad4980
mzelder <maciej.zelder@wp.pl>
2025-12-06 13:05:29 +0100

    demo + created boilerplate of thje class

---
+++ anonimizer.py
@@ -0,0 +1,15 @@
+class Anonimizer:
+    def __init__(self,  text=None, file_name=None):
+        if text:
+            self.text = self.load_text(text)
+        elif file_name:
+            self.text = self.load_file(file_name)
+        else:
+            raise ValueError("Either 'file' or 'text' must be provided")
+        
+    def load_file(self, file):
+        with open(file, "r", encoding="utf-8") as f:
+            return f.read()
+
+    def load_text(self, text):
+        return text
